ff8a95c42bbec394648bf895c08f073821c98bef
entosen <entosen@gmail.com>
2023-02-25 18:43:22 +0900

    MacのATOKにローマ字を流し込むスクリプトを改良

--- src/generate_applescript.py
+++ src/generate_applescript.py
@@ -14,7 +14,20 @@ def main():
     with open(args.input, encoding='cp932') as f:
         stroke_dict = atok_romaji_table.table2records(f)
 
-    # print(stroke_dict)
+
+    print(textwrap.dedent('''\
+        -- あらかじめ、下記の設定を ON にしておく
+        -- - システム環境設定 > キーボード > ショートカット > コントロール間のフォーカス移動をキーボードで操作 > ON
+        --
+        -- スクリプトエディタを起動し、このスクリプトを開く (まだ実行しない)
+        -- 
+        -- ATOK キー・ローマ字カスタマイザを起動
+        -- ローマ字テーブルが空のスタイルを1つ用意する
+        -- 「ローマ字設定ボタン」
+        -- Tabキーを何度か押して、「＋」ボタンにフォーカスがある状態にする
+        -- 
+        -- スクリプトエディタで、このスクリプトを実行
+        '''))
 
     print(textwrap.dedent('''\
         tell application "ATOK Customizer"
@@ -22,26 +35,30 @@ def main():
         end tell
         '''))
 
-    print('tell application "System Events"')
+    print(textwrap.dedent('''\
+        on addRomaji(roman, kana)
+            tell application "System Events"
+                
+                key code 49 -- Space で "+" ボタン押下
+                delay 0.25
+                
+                -- ローマ字の入力欄にフォーカスがある
+                keystroke roman
+                
+                key code 48 -- Tab で かな の入力欄にフォーカスを移動
+                set the clipboard to kana
+                keystroke "v" using {command down}
+                delay 0.25
+                
+                key code 36 -- Enter
+                delay 0.25
+                
+            end tell
+        end addRomaji
+        '''))
 
     for k,v in stroke_dict.items():
-        print(textwrap.dedent('''\
-            key code 49 -- Space
-            set the clipboard to "%s"
-            keystroke "v" using {command down}
-            delay 0.5
-            
-            key code 48 -- Tab
-            set the clipboard to "%s"
-            keystroke "v" using {command down}
-            delay 0.5
-            
-            key code 36 -- Enter
-            delay 0.5
-
-            ''') % (k,v))
-
-    print('end tell')
+        print(f'addRomaji("{k}", "{v}")')
 
 if __name__=='__main__':
     main()
